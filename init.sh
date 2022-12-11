950ef5b695266aa5b233d5af4670b76505a3ff74
allania7med11 <allania7med11@gmail.com>
2022-12-11 17:47:54 +0000

    run shop_back  in detached mode

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@ Run () {
    sudo chmod +x $1 && $1
 }
 
-Run /vagrant/ngnix/setup.sh
-Run /vagrant/config/docker/setup.sh
-Run /vagrant/config/db/setup.sh
-Run /vagrant/config/shop_back/setup.sh
+# Run /vagrant/ngnix/setup.sh
+# Run /vagrant/config/docker/setup.sh
+# Run /vagrant/config/db/setup.sh
+# Run /vagrant/config/shop_back/setup.sh
 Run /vagrant/config/shop_front/setup.sh
 Run /vagrant/ngnix/update.sh
