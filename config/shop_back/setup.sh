5f4091e2e1c979c72aafb27dce16a4ec2ad39b8b
allania7med11 <allania7med11@gmail.com>
2022-12-11 21:01:39 +0000

    Add ssl

--- config/shop_back/setup.sh
+++ config/shop_back/setup.sh
@@ -6,7 +6,7 @@ sudo -u postgres psql shop_db < /vagrant/apps/shop_back/shop_db.sql
 echo "Load and run shop_back image"
 sudo docker load < /vagrant/apps/shop_back/shop_back.tar
 sudo kill -9 $(sudo lsof -t -i:8000)
-sudo docker run -d --network=host --env-file /vagrant/config/shop_back/.env shop_back:v1 8000
+sudo docker run -d  --network=host --env-file /vagrant/config/shop_back/.env --restart unless-stopped shop_back:v1 8000
 echo "Add shop_back static files"
 sudo rm -fr /var/www/shop_back_static/*
 sudo cp -a /vagrant/apps/shop_back/static/. /var/www/shop_back_static/
--- ngnix/ssl.sh
+++ ngnix/ssl.sh
@@ -1,8 +1,16 @@
+source /vagrant/.env
+echo "Installing Certbot"
 sudo snap install core; sudo snap refresh core
-sudo apt remove certbot -y
+sudo apt remove certbot
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo nginx -t
-sudo certbot --nginx -d testingapp.live -d www.testingapp.live
+echo "Allowing HTTPS Through the Firewall"
+sudo ufw enable
+sudo ufw allow 'Nginx Full'
+sudo ufw delete allow 'Nginx HTTP'
+sudo ufw status
+echo "Obtaining an SSL Certificate"
+sudo certbot --nginx -d ${DOMAIN} -d www.${DOMAIN}
+echo "Verifying Certbot Auto-Renewal"
 sudo systemctl status snap.certbot.renew.service
 sudo certbot renew --dry-run
