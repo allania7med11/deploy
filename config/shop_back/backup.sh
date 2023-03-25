186b9a3a42d62f5b9b382c3ebb74a384aefc2d67
allania7med11 <allania7med11@gmail.com>
2023-03-25 19:58:42 +0000

    ignore sql files

--- config/shop_back/backup.sh
+++ config/shop_back/backup.sh
@@ -1,2 +1,2 @@
 echo "Backup shop_db at $(date +"%Y_%m_%d_%I_%M_%p")"
-sudo -u postgres pg_dump shop_db > $(pwd)/apps/shop_back/backups/shop_db_$(date +"%Y_%m_%d_%I_%M_%p").sql
+sudo -u postgres pg_dump shop_db > $(pwd)/config/shop_back/shop_db_$(date +"%Y_%m_%d_%I_%M_%p").sql
