85d9a421a5ceea206bb8ded1210423719289c2e3
allania7med11 <allania7med11@gmail.com>
2023-10-18 09:08:40 +0100

    update setup

--- config/docker/setup.sh
+++ config/docker/setup.sh
@@ -1,19 +1,21 @@
-# sudo chmod +x /vagrant/config/shop_back/setup.sh
-echo "Install Docker Engine on Ubuntu"
-sudo apt-get remove docker docker-engine docker.io containerd runc 
-sudo apt-get update 
-sudo apt-get upgrade -y
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release 
-sudo apt autoremove -y
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#!/bin/bash
+
+echo "****Install Docker****"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-sudo docker run hello-world
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "****Adjust permissions for Docker socket****"
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker.service
+
+echo "****Install Docker Compose****"
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "****Display Docker version****"
+docker --version
+docker-compose --version
---
+++ config/shop_back/setup_dev.sh
@@ -0,0 +1,12 @@
+echo "****Create shop_db and load data****"
+source /vagrant/config/shop_back/.env
+sudo -u postgres dropdb shop_db
+sudo -u postgres createdb shop_db
+sudo -u postgres psql shop_db < /vagrant/apps/shop_back/shop_db_init.sql
+echo "****Load and run shop_back image****"
+docker pull allania7med11/shop_back:latest
+sudo kill -9 $(sudo lsof -t -i:8000)
+sudo docker run -d  --network=host --env-file /vagrant/config/shop_back/.env --restart unless-stopped shop_back:latest 8000
+echo "****Add shop_back static files****"
+sudo rm -fr /var/www/shop_back_static/*
+sudo cp -a /vagrant/apps/shop_back/static/. /var/www/shop_back_static/
